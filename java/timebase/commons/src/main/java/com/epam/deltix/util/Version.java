3125d9f75c19ee64144e5841bec828ba4cf1d5ea
Alex Karpovich <alexander.karpovich@gmail.com>
2026-06-25 21:07:57 +0300

    [*] CI

--- java/timebase/commons/src/main/java/com/epam/deltix/util/Version.java
+++ java/timebase/commons/src/main/java/com/epam/deltix/util/Version.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2023 EPAM Systems, Inc
- *
- * See the NOTICE file distributed with this work for additional information
- * regarding copyright ownership. Licensed under the Apache License,
- * Version 2.0 (the "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package com.epam.deltix.util;
 
 import com.epam.deltix.util.io.Home;
@@ -31,11 +15,11 @@ public abstract class Version {
 
     private static final Log            LOG = LogFactory.getLog(Version.class);
     public static final int             MAJOR = 6;
-    public static final int             MINOR = 0;
-    public static final String          NAME = "12-SNAPSHOT";
-    public static final String          BUILD = "35e64ee76d";
+    public static final int             MINOR = 2;
+    public static final String          NAME = "18-SNAPSHOT";
+    public static final String          BUILD = "609e19fa";
     public static final Integer         COMMITS_AFTER_TAG = null;
-    public static final String          BUILD_DATE = "2021-02-15 11:04:03 +0300";
+    public static final String          BUILD_DATE = "2026-06-25 19:02:17 +0300";
 
     public static final String          VERSION_STRING;
     public static final String          MAJOR_VERSION_STRING;
@@ -96,6 +80,6 @@ public abstract class Version {
     }
 
     public static void                  main (String [] args) {
-        System.out.println ("EPAM Software - Version " + VERSION_STRING);
+        System.out.println ("Deltix Software - Version " + VERSION_STRING);
     }
 }

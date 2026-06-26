57f8f250e24af59fc8c101596ff63f6be676f10e
Alex Karpovich <alexander.karpovich@gmail.com>
2026-06-26 11:47:45 +0300

    [*] fix gradle tasks dependencies

--- java/timebase/commons/src/main/java/com/epam/deltix/util/Version.java
+++ java/timebase/commons/src/main/java/com/epam/deltix/util/Version.java
@@ -17,9 +17,9 @@ public abstract class Version {
     public static final int             MAJOR = 6;
     public static final int             MINOR = 2;
     public static final String          NAME = "18-SNAPSHOT";
-    public static final String          BUILD = "3125d9f7";
+    public static final String          BUILD = "2c3be72f";
     public static final Integer         COMMITS_AFTER_TAG = null;
-    public static final String          BUILD_DATE = "2026-06-25 21:07:57 +0300";
+    public static final String          BUILD_DATE = "2026-06-25 21:23:51 +0300";
 
     public static final String          VERSION_STRING;
     public static final String          MAJOR_VERSION_STRING;
